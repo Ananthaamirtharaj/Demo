e2c3bdc39c577e994207f8c1671ee19c51d11ea7
Anantha Amirtharaj Rapheal <Anantha.Rapheal@fadv.com>
2019-06-20 15:36:56 +0530

    jenkinsbranch.java file added and modified

---
+++ jenkinsbranch.java
@@ -0,0 +1,6 @@
+class jenkinsbranch {
+  public static void main(String[] arguments) {
+    System.out.println("Let's do something using Java technology.");
+	System.out.println("Jenkins Java.");
+  }
+}
